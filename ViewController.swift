5ddb15bb9071d6e7707a5513335f4b723ca57bf6
“Robert <rob.h.gilmore@gmail.com>
2015-11-10 17:49:00 -0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Robert Gilmore on 11/10/15.
+//  Copyright © 2015 Tike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    var activePlayer = 1 // 1 = noughts 2 = crosses
+    
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        sender.setImage(UIImage(named: "cross.png"), forState: .Normal)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
