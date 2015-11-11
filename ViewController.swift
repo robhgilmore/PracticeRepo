7658e34f4c207c223b98ac545fdfad352a3b11f7
“Robert <rob.h.gilmore@gmail.com>
2015-11-10 17:58:21 -0700

    Third commit

--- ViewController.swift
+++ ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     }
     
     // doesnt matter what i've done
-    
+    // I'm adding another comment to see if the commit happens immediately. 
     
     
     
